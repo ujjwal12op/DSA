684219469054f226bc30223006601fde870f78a2
Ujjwal Tiwari <ujtiwari@gmail.com>
2023-01-23 15:27:37 +0530

    added difference

---
+++ diff.java
@@ -0,0 +1,8 @@
+public class diff {
+    public static void main(String args[])   
+{  
+int n1 = 225, n2 = 115, diff;  
+diff = n1 - n2;  
+System.out.println("The sum of numbers is: "+diff);  
+}  
+}
