c44d92fe150400cfe3196bc8344f3fefcb3b6931
Ujjwal Tiwari <ujtiwari@gmail.com>
2023-01-23 15:24:10 +0530

    array updated

--- Array.java
+++ Array.java
@@ -1,1 +1,9 @@
-ls
+public class Array 
+{  
+public static void main(String args[])   
+{  
+int n1 = 225, n2 = 115, sum;  
+sum = n1 + n2;  
+System.out.println("The sum of numbers is: "+sum);  
+}  
+}  
